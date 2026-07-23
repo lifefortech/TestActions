f39b7d03a3f84021fda68e10ccb0c0fbaf01bbf5
ZHFighting <1658938409@qq.com>
2026-07-24 02:26:36 +0800

    修改格式

--- test_calculator.py
+++ test_calculator.py
@@ -19,15 +19,13 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(multiply(3, 4), 12)
         self.assertEqual(multiply(-2, 3), -6)
 
-<<<<<<< HEAD
     def test_is_equal(self) -> None:
         self.assertTrue(is_equal(2, 2))
         self.assertFalse(is_equal(3, 4))
-=======
+        
     def test_divide(self) -> None:
         self.assertEqual(divide(2, 1), 2)
         self.assertEqual(divide(6, 3), 2)
->>>>>>> origin/main
 
 
 if __name__ == "__main__":
